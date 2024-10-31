a95226b748010d77ecaa644a7d3a65fae87f70a6
RameesAP <109672661+RameesAP@users.noreply.github.com>
2024-11-01 01:13:48 +0530

    Time: 0 ms (100.00%), Space: 49.3 MB (15.68%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,18 @@
+let nums = [3, 2, 2, 3];
+let val = 3;
+
+function removeElement(nums,val){
+    let k=0;
+    
+    for(let i =0; i <nums.length; i++){
+        if(nums[i] !== val){
+            nums[k] = nums[i];
+            k++;
+        }
+    } 
+    return k;
+}
+
+let result = removeElement(nums,val);
+console.log(nums);
+// console.log(result)
